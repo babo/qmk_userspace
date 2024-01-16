b59886b4504f0083bc639b0f507af66a8c0dbeb0
Attila Babo <attila.babo@gmail.com>
2024-01-16 13:16:15 +0100

    Set tapping term separately for each keyboard

--- keyboards/ferris/keymaps/babo/rules.mk
+++ keyboards/ferris/keymaps/babo/rules.mk
@@ -4,4 +4,3 @@ MOUSEKEY_ENABLE = yes	# Mouse keys(+4700)
 NKRO_ENABLE = yes		# USB Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
 DYNAMIC_TAPPING_TERM_ENABLE = yes
 MIRYOKU_KLUDGE_THUMBCOMBOS = yes
-COMBO_ENABLE = no
