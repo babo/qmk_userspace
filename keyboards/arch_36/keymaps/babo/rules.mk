35baadbe9bb4267541478ad3b62f386d205ed4b1
Attila Babo <attila.babo@gmail.com>
2025-09-27 00:26:03 +0200

    obosob fix

---
+++ keyboards/arch_36/keymaps/babo/rules.mk
@@ -0,0 +1,5 @@
+BOOTMAGIC_ENABLE = yes	# Virtual DIP switch configuration(+1000)
+EXTRAKEY_ENABLE = yes	# Audio control and System control(+450)
+MOUSEKEY_ENABLE = yes	# Mouse keys(+4700)
+NKRO_ENABLE = yes		# USB Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+RGBLIGHT_ENABLE = yes
