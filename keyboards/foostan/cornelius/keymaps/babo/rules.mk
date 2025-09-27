4a87fe7c0286a1ed997160e187e47f2870b45963
Attila Babo <attila.babo@gmail.com>
2025-09-27 15:53:36 +0200

    Update keyboards to build with simplified config

--- keyboards/foostan/cornelius/keymaps/babo/rules.mk
+++ keyboards/foostan/cornelius/keymaps/babo/rules.mk
@@ -1,3 +1,8 @@
+AVR_USE_MINIMAL_PRINTF = yes
+COMMAND_ENABLE = no
+CONSOLE_ENABLE = no
+LTO_ENABLE = yes # enable link time optimization
+UNICODE_COMMON = yes
 UNICODEMAP_ENABLE = yes
 
 SRC += hungarian.c
--- keyboards/gergo/keymaps/babo/keymap.c
+++ keyboards/gergo/keymaps/babo/keymap.c
@@ -1,1 +1,2 @@
 // placeholder
+#include "hungarian.h"
