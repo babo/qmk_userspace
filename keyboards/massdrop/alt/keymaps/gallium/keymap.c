617d00122291ecbb9b76798790d736cb5b7612e7
Attila Babo <attila.babo@gmail.com>
2025-09-27 20:59:20 +0200

    Change drop/alt layout like HHKB

--- keyboards/massdrop/alt/keymaps/gallium/keymap.c
+++ keyboards/massdrop/alt/keymaps/gallium/keymap.c
@@ -8,16 +8,17 @@ enum alt_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_65_ansi_blocker(
-        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_CAPS,
-        KC_TAB,  KC_B,    KC_L,    KC_D,    KC_C,    KC_V,    KC_J,    KC_Y,    KC_O,    KC_U,    KC_COMM, KC_LBRC, KC_RBRC, KC_BSLS, KC_HOME,
-        KC_DEL , KC_N,    KC_R,    KC_T,    KC_S,    KC_G,    KC_P,    KC_H,    KC_A,    KC_E,    KC_I,    KC_QUOT,          KC_ENT,  KC_PGUP,
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_ESC,
+        KC_TAB,  KC_B,    KC_L,    KC_D,    KC_C,    KC_V,    KC_J,    KC_Y,    KC_O,    KC_U,    KC_COMM, KC_LBRC, KC_RBRC, KC_BSPC, KC_HOME,
+        KC_LGUI, KC_N,    KC_R,    KC_T,    KC_S,    KC_G,    KC_P,    KC_H,    KC_A,    KC_E,    KC_I,    KC_SCLN,          KC_ENT,  KC_PGUP,
         KC_LSFT, KC_X,    KC_Q,    KC_M,    KC_W,    KC_Z,    KC_K,    KC_F,    KC_QUOT, KC_SLSH, KC_DOT,  KC_RSFT,          KC_UP,   KC_PGDN,
-        KC_LGUI, KC_LCTL, KC_LALT,                            KC_SPC,                             KC_RALT, MO(1),   KC_LEFT, KC_DOWN, KC_RGHT
+        KC_LCTL, KC_LALT, KC_LGUI,                            KC_SPC,                             KC_RGUI, MO(1),   KC_LEFT, KC_DOWN, KC_RGHT
+
     ),
     [1] = LAYOUT_65_ansi_blocker(
-        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_MUTE,
+        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_MUTE,
         _______, RGB_SPD, RGB_VAI, RGB_SPI, RGB_HUI, RGB_SAI, _______, _______,_______,_______,   KC_PSCR, KC_SCRL, KC_PAUS, _______, KC_END,
-        _______, RGB_RMOD,RGB_VAD, RGB_MOD, RGB_HUD, RGB_SAD, _______, _______, _______, _______, _______, _______,          _______, KC_VOLU,
+        KC_CAPS, RGB_RMOD,RGB_VAD, RGB_MOD, RGB_HUD, RGB_SAD, _______, _______, _______, _______, _______, _______,          _______, KC_VOLU,
         _______, RGB_TOG, _______, _______, _______, QK_BOOT, NK_TOGG, _______, _______, _______, _______, _______,          KC_PGUP, KC_VOLD,
         _______, _______, _______,                            _______,                            _______, _______, KC_HOME, KC_PGDN, KC_END
     )
