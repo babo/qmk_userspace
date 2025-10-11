6f1d24a01178c259ff2eca017fb2f69213e1860e
Attila Babo <attila.babo@gmail.com>
2025-10-12 01:34:23 +0200

    swap gui

--- keyboards/massdrop/alt/keymaps/gallium/keymap.c
+++ keyboards/massdrop/alt/keymaps/gallium/keymap.c
@@ -12,7 +12,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_B,    KC_L,    KC_D,    KC_C,    KC_V,    KC_Z,    KC_Y,    KC_O,    KC_U,    KC_COMM, KC_LBRC, KC_RBRC, KC_BSPC, KC_HOME,
         KC_LGUI, KC_N,    KC_R,    KC_T,    KC_S,    KC_G,    KC_P,    KC_H,    KC_A,    KC_E,    KC_I,    KC_SCLN,          KC_ENT,  KC_PGUP,
         KC_LSFT, KC_X,    KC_Q,    KC_M,    KC_W,    KC_J,    KC_K,    KC_F,    KC_QUOT, KC_SLSH, KC_DOT,  KC_RSFT,          KC_UP,   KC_PGDN,
-        KC_LCTL, KC_LALT, KC_LGUI,                            KC_SPC,                             KC_RGUI, MO(1),   KC_LEFT, KC_DOWN, KC_RGHT
+        KC_LGUI, KC_LALT, KC_LCTL,                            KC_SPC,                             KC_RGUI, MO(1),   KC_LEFT, KC_DOWN, KC_RGHT
 
     ),
     [1] = LAYOUT_65_ansi_blocker(
