effe01378a9fc753c0340c6b39a693e0048b88d8
Attila Babo <attila.babo@gmail.com>
2026-07-12 14:48:02 +0200

    Support both crkbd in keymap

--- keyboards/crkbd/keymaps/babo/config.h
+++ keyboards/crkbd/keymaps/babo/config.h
@@ -10,6 +10,20 @@
 
 #define PERMISSIVE_HOLD
 
+#if defined(KEYBOARD_crkbd_rev4_1_standard)
+#define LAYOUT_miryoku( \
+       K00,   K01,   K02,   K03,   K04,   K05,   K06,   K07,   K08,   K09, \
+       K10,   K11,   K12,   K13,   K14,   K15,   K16,   K17,   K18,   K19, \
+       K20,   K21,   K22,   K23,   K24,   K25,   K26,   K27,   K28,   K29, \
+       N30,   N31,   K32,   K33,   K34,   K35,   K36,   K37,   N38,   N39 \
+) \
+LAYOUT_split_3x6_3_ex2( \
+DT_PRNT, K00,   K01,   K02,   K03,   K04,   KC_NO,   KC_NO, K05,   K06,   K07,   K08,   K09,   DT_UP,  \
+KC_LSFT, K10,   K11,   K12,   K13,   K14,   KC_NO,   KC_NO, K15,   K16,   K17,   K18,   K19,   KC_RSFT, \
+KC_NO,   K20,   K21,   K22,   K23,   K24,                   K25,   K26,   K27,   K28,   K29,   DT_DOWN, \
+                       K32,   K33,   K34,                   K35,   K36,   K37 \
+)
+#else
 #define LAYOUT_miryoku( \
        K00,   K01,   K02,   K03,   K04,   K05,   K06,   K07,   K08,   K09, \
        K10,   K11,   K12,   K13,   K14,   K15,   K16,   K17,   K18,   K19, \
@@ -22,3 +36,4 @@ KC_LSFT, K10,   K11,   K12,   K13,   K14,   K15,   K16,   K17,   K18,   K19,   K
 KC_NO,   K20,   K21,   K22,   K23,   K24,   K25,   K26,   K27,   K28,   K29,   DT_DOWN, \
                        K32,   K33,   K34,   K35,   K36,   K37 \
 )
+#endif
